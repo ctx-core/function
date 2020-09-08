1285865f5f7caa221217c2f97551a5cb7beb8bca
Brian Takita <brian.takita@gmail.com>
2020-09-07 23:45:28 -0700

    feat: + _andand_or

--- _one_andand_or.ts
+++ _one_andand_or.ts
@@ -5,6 +5,7 @@ import { andand_or } from './andand_or'
 export function _one_andand_or(name_a1, or_fn) {
 	return (obj)=>andand_or(obj, name_a1, or_fn)
 }
+export const _andand_or = _one_andand_or
 export const _andand__or = _one_andand_or
 export const _andand_or__one = _one_andand_or
 export const _andand__or__one = _one_andand_or
