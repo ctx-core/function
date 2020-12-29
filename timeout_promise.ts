d5ab649c911778b47c8223ad84bf0328e53bd943
Brian Takita <brian.takita@gmail.com>
2020-12-29 16:46:11 -0500

    fix: timeout_promise: let id: Timeout type declaration

--- timeout_promise.ts
+++ timeout_promise.ts
@@ -1,9 +1,10 @@
+import type { Timeout } from './Timeout'
 /**
  * @see [link: https://italonascimento.github.io/applying-a-timeout-to-your-promises/]
  * @see [link: http://disq.us/p/1k8w63m]
  */
 export function timeout_promise<O extends unknown = unknown>(ms: number, promise: Promise<O>): Promise<O> {
-  let id
+  let id: Timeout
   let timeout = new Promise((_resolve, reject) => {
     id = setTimeout(() => {
       reject(`Timed out in ${ms}ms.`)
