3a3ad86bfc91e5e980bb8595fc7e6af9612e5546
Brian Takita <brian.takita@gmail.com>
2022-01-20 22:47:51 -0600

    promise_timeout: formatting

--- src/promise_timeout.js
+++ src/promise_timeout.js
@@ -1,8 +1,7 @@
 export async function promise_timeout(promise, ms, error = new Error(`Timed out after ${ms}ms.`)) {
 	let id
 	let timeout = new Promise((_resolve, reject)=>{
-		id = setTimeout(()=>reject(error)
-			, ms)
+		id = setTimeout(()=>reject(error), ms)
 	})
 	return Promise.race([
 		typeof promise === 'function' ? promise() : promise,
