5177e6ace0ba7b76b3ad167dfab7277d93876d32
Brian Takita <brian.takita@gmail.com>
2023-02-11 02:47:47 -0500

    formatting

--- src/wrap_aa_/index.js
+++ src/wrap_aa_/index.js
@@ -3,8 +3,7 @@ import { wrap_a_ } from '../wrap_a_/index.js'
  * Returns `wrap_aa_T` wrapped as a 2-dimensional array
  */
 export function wrap_aa_(aa_nowrap) {
-	return wrap_a_(aa_nowrap).map((a_nowrap)=>wrap_a_(a_nowrap)
-	)
+	return wrap_a_(aa_nowrap).map(a_nowrap=>wrap_a_(a_nowrap))
 }
 export {
 	wrap_aa_ as wrap_a2_,
